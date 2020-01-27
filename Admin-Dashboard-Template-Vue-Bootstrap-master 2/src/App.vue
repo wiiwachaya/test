25e860a6a38d6a8210e405a39f26afb8f38e7f34
wiiwachaya <wiiwachaya001@gmail.com>
2020-01-27 15:55:00 +0700

    up bar

--- Admin-Dashboard-Template-Vue-Bootstrap-master 2/src/App.vue
+++ Admin-Dashboard-Template-Vue-Bootstrap-master 2/src/App.vue
@@ -23,33 +23,15 @@
     <div class="sidebar-fixed position-fixed">
       <a class="logo-wrapper"><img alt="" class="img-fluid" src="./assets/logo-mdb-vue-small.png"/></a>
       <mdb-list-group class="list-group-flush">
-        <router-link to="/dashboard" @click.native="activeItem = 1">
-          <mdb-list-group-item :action="true" :class="activeItem === 1 && 'active'"><mdb-icon icon="chart-pie" class="mr-3"/>Dashboard</mdb-list-group-item>
-        </router-link>
-        <router-link to="/profile" @click.native="activeItem = 2">
-          <mdb-list-group-item :action="true" :class="activeItem === 2 && 'active'"><mdb-icon icon="user" class="mr-3"/>Profile</mdb-list-group-item>
-        </router-link>
-        <router-link to="/tables"  @click.native="activeItem = 3">
-          <mdb-list-group-item :action="true" :class="activeItem === 3 && 'active'"><mdb-icon icon="table" class="mr-3"/>Tables</mdb-list-group-item>
-        </router-link>
-        <router-link to="/maps" @click.native="activeItem = 4">
-          <mdb-list-group-item :action="true" :class="activeItem === 4 && 'active'"><mdb-icon icon="map" class="mr-3"/>Maps</mdb-list-group-item>
-        </router-link>
-        <router-link to="/task" @click.native="activeItem = 5">
-          <mdb-list-group-item :action="true" :class="activeItem === 5 && 'active'"><mdb-icon icon="map" class="mr-3"/>Task</mdb-list-group-item>
-        </router-link>
-        <router-link to="/404" @click.native="activeItem = 6">
-          <mdb-list-group-item :action="true" :class="activeItem === 6 && 'active'"><mdb-icon icon="exclamation" class="mr-3"/>404</mdb-list-group-item>
-        </router-link>
         <router-link to="/task_work" @click.native="activeItem = 7">
-          <mdb-list-group-item :action="true" :class="activeItem === 7 && 'active'"><mdb-icon icon="exclamation" class="mr-3"/>404</mdb-list-group-item>
+          <mdb-list-group-item :action="true" :class="activeItem === 7 && 'active'"><mdb-icon icon="calendar-check" class="mr-3"/>Task</mdb-list-group-item>
         </router-link>
       </mdb-list-group>
     </div>
     <!-- /Sidebar  -->
     <main>
       <div class="mt-5 p-5">
-        <router-view></router-view>
+        <router-view/>
       </div>
       <ftr color="primary-color-dark" class="text-center font-small darken-2">
         <div class="pt-4">
