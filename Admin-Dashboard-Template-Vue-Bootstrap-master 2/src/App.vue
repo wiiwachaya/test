2428892d880e73ad70c9888a5f986b4bd96ea8a3
wiiwachaya <wiiwachaya001@gmail.com>
2020-01-27 17:09:57 +0700

    bar2.1

--- Admin-Dashboard-Template-Vue-Bootstrap-master 2/src/App.vue
+++ Admin-Dashboard-Template-Vue-Bootstrap-master 2/src/App.vue
@@ -27,10 +27,6 @@
         <router-view/>
       </div>
       <ftr color="primary-color-dark" class="text-center font-small darken-2">
-        <div class="pt-4">
-          <mdb-btn outline="white" tag="a" href="https://mdbootstrap.com/docs/vue/getting-started/download/" target="_blank">Download MDB <mdb-icon icon="download" class="ml-2"/></mdb-btn>
-          <mdb-btn outline="white" tag="a" href="https://mdbootstrap.com/education/bootstrap/" target="_blank">Start free tutorial <mdb-icon icon="graduation-cap" class="ml-2"/></mdb-btn>
-        </div>
         <hr class="my4"/>
         <div class="pb-4">
           <a href="#"><mdb-icon fab icon="facebook-square" class="mr-3"/></a>
@@ -43,7 +39,7 @@
           <a href="#"><mdb-icon fab icon="codepen" class="mr-3"/></a>
         </div>
         <p class="footer-copyright mb-0 py-3 text-center">
-          &copy; {{new Date().getFullYear()}} Copyright: <a href="https://mdbootstrap.com/docs/vue/"> MDBootstrap.com </a>
+          &copy; {{new Date().getFullYear()}} Copyright:
         </p>
       </ftr>
     </main>
