da1c861c1ee9b6d704713e9dd9f736cd17a22789
nesara123 <mai-some-one@hotmail.com>
2020-02-03 11:51:06 +0700

    up Home

--- Admin-Dashboard-Template-Vue-Bootstrap-master 2/src/view/Home.vue
+++ Admin-Dashboard-Template-Vue-Bootstrap-master 2/src/view/Home.vue
@@ -1,15 +1,74 @@
 <template>
   <div>
-    Home
-  </div>
+    <div class="input-group md-form form-sm form-1 pl-0">
+      <div class="input-group-prepend">
+        <span class="input-group-text cyan lighten-2" id="basic-text1">
+          <mdbIcon icon="search"/>
+        </span>
+      </div>
+      <input class="form-control my-0 py-1" type="text" placeholder="ข่าวสารวันนี้" aria-label="Search">
+    </div>
+    <mdb-row>
+      <mdb-col><mdb-btn active size="lg" color=" blue-gradient"><mdb-icon icon="user" class="mr-3"/>ยินดีต้อนรับ</mdb-btn></mdb-col>
+      <mdb-col><mdb-btn active size="lg" color=" purple-gradient"><mdb-icon icon="calendar-alt" class="mr-3"/>กิจกรรมล่าสุด</mdb-btn></mdb-col>
+      <mdb-col><mdb-btn active size="lg" color=" peach-gradient"><mdb-icon icon="calendar-check" class="mr-3"/>งานล่าสุด</mdb-btn></mdb-col>
+      <mdb-col><mdb-btn active size="lg" color="  purple-gradient"><mdb-icon icon="address-book" class="mr-3"/>ผู้ติดต่อล่าสุด</mdb-btn></mdb-col>
+    </mdb-row>
+    <mdb-col>
+      <h2>กิจกรรมที่ต้องเข้าร่วม</h2>
+    </mdb-col>
+    <mdb-btn tag="a" floating action class="ml-auto mr-4 mdb-color lighten-4">
+      <mdb-icon icon="chevron-left" class="pl-2" />
+    </mdb-btn>
+    <mdb-btn tag="a" floating action class="ml-auto mr-9 mdb-color lighten-4">
+      <mdb-icon icon="chevron-right" class="pl-2" />
+    </mdb-btn>
+    <mdb-masonry :numCols="3" :maxHeight="1000">
+      <mdb-masonry-item :order="1" :itemStyle="{'height': '250px'}">ผู้เข้าร่วม</mdb-masonry-item>
+      <mdb-masonry-item :order="2" :itemStyle="{'height': '250px'}">ผู้เข้าร่วม</mdb-masonry-item>
+      <mdb-masonry-item :order="0" :itemStyle="{'height': '250px'}">ผู้เข้าร่วม</mdb-masonry-item>
+    </mdb-masonry>
+    </div>
 </template>
 
 <script>
+import { mdbIcon, mdbBtn, mdbBtnGroup, mdbBtnToolbar, mdbContainer, mdbRow, mdbCol, mdbMasonry, mdbMasonryItem } from 'mdbvue'
 export default {
-  name: 'Home'
+  components: {
+    mdbIcon,
+    mdbBtn,
+    mdbBtnGroup,
+    mdbBtnToolbar,
+    mdbContainer,
+    mdbRow,
+    mdbCol,
+    mdbMasonry,
+    mdbMasonryItem
+  }
 }
 </script>
 
 <style scoped>
+  .input-group.md-form.form-sm.form-1 input{
+    border: 1px solid #bdbdbd;
+    border-top-right-radius: 0.25rem;
+    border-bottom-right-radius: 0.25rem;
+  }
+  .input-group.md-form.form-sm.form-2 input {
+    border: 1px solid #bdbdbd;
+    border-top-left-radius: 0.25rem;
+    border-bottom-left-radius: 0.25rem;
+  }
+
+  .masonry-with-columns div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #bbdefb;
+    color: white;
+    margin: 0.5rem;
+    font-weight: 900;
+    font-size: 2rem;
+  }
 
 </style>
